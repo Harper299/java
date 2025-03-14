2d9d12d4a9944bc6c95f5e4982d89f8bead78f69
Harper299 <masschandru25@gmail.com>
2025-03-14 19:18:34 +0530

    Create PrimeNumbers.java

---
+++ PrimeNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner; 
+public class PrimeNumbersList 
+ { 
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter an integer: "); 
+        int limit = scanner.nextInt(); 
+        System.out.println("Prime numbers up to " + limit + ":"); 
+        for (int num = 2; num <= limit; num++) { 
+            if (isPrime(num)) { 
+                System.out.print(num + " "); 
+            } 
+        } 
+        scanner.close(); 
+    }     
+    public static boolean isPrime(int num) { 
+        if (num <= 1) { 
+            return false; 
+        } 
+        for (int i = 2; i <= Math.sqrt(num); i++) { 
+            if (num % i == 0) { 
+       return false; 
+            } 
+        } 
+        return true; 
+    } 
+}
